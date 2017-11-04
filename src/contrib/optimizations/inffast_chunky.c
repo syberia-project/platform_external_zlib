699c0dd9e675aee8be55670e809da2ad1c851d9d
Adenilson Cavalcanti <adenilson.cavalcanti@arm.com>
2017-11-03 17:49:46 -0700

    zlib: Fix InflateBack corner case
    This handles the case where a zlib user could rely on InflateBack
    API to decompress content.
    
    The NEON optimization assumes that it can perform wide stores, sometimes
    overwriting data on the output pointer (but never overflowing the buffer
    end as it has enough room for the write).
    
    For infback there is no such guarantees (i.e. no extra wiggle room),
    which can result in illegal operations. This patch fixes the potential
    issue by falling back to the non-optimized code for such cases.
    
    Also it adds some comments about the entry assumptions in inflate and
    writes out a defined value at the write buffer to identify where
    the real data has ended (helpful while debugging).
    
    Bug: 769880
    Change-Id: I08130e4d012e55fb1a114394b88df7e9d8a6542f
    Reviewed-on: https://chromium-review.googlesource.com/749732
    Reviewed-by: Chris Blume <cblume@chromium.org>
    Commit-Queue: Adenilson Cavalcanti <cavalcantii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513830}
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- src/contrib/optimizations/inffast_chunky.c
+++ src/contrib/optimizations/inffast_chunky.c
@@ -6,7 +6,7 @@
 #include "../../zutil.h"
 #include "../../inftrees.h"
 #include "../../inflate.h"
-#include "../../inffast.h"
+#include "inffast_chunky.h"
 #include "chunkcopy.h"
 
 #ifdef ASMINF
@@ -28,6 +28,10 @@
         strm->avail_out >= 258
         start >= strm->avail_out
         state->bits < 8
+        strm->next_out[0..strm->avail_out] does not overlap with
+              strm->next_in[0..strm->avail_in]
+        strm->state->window is allocated with an additional
+              CHUNKCOPY_CHUNK_SIZE-1 bytes of padding beyond strm->state->wsize
 
    On return, state->mode is one of:
 
@@ -46,7 +50,7 @@
       requires strm->avail_out >= 258 for each loop to avoid checking for
       output space.
  */
-void ZLIB_INTERNAL inflate_fast(strm, start)
+void ZLIB_INTERNAL inflate_fast_chunky(strm, start)
 z_streamp strm;
 unsigned start;         /* inflate()'s starting value for strm->avail_out */
 {
--- src/contrib/optimizations/inflate.c
+++ src/contrib/optimizations/inflate.c
@@ -83,7 +83,7 @@
 #include "../../zutil.h"
 #include "../../inftrees.h"
 #include "../../inflate.h"
-#include "../../inffast.h"
+#include "inffast_chunky.h"
 #include "chunkcopy.h"
 #if defined(USE_ARMV8_CRC32)
 #include "arm/arm_features.h"
@@ -1063,7 +1063,7 @@ int flush;
         case LEN:
             if (have >= 6 && left >= 258) {
                 RESTORE();
-                inflate_fast(strm, out);
+                inflate_fast_chunky(strm, out);
                 LOAD();
                 if (state->mode == TYPE)
                     state->back = -1;
@@ -1269,6 +1269,16 @@ int flush;
        Note: a memory error from inflate() is non-recoverable.
      */
   inf_leave:
+   /* We write a defined value in the unused space to help mark
+    * where the stream has ended. We don't use zeros as that can
+    * mislead clients relying on undefined behavior (i.e. assuming
+    * that the data is over when the buffer has a zero/null value).
+    */
+   if (left >= CHUNKCOPY_CHUNK_SIZE)
+      memset(put, 0x55, CHUNKCOPY_CHUNK_SIZE);
+   else
+      memset(put, 0x55, left);
+
     RESTORE();
     if (state->wsize || (out != strm->avail_out && state->mode < BAD &&
             (state->mode < CHECK || flush != Z_FINISH)))
